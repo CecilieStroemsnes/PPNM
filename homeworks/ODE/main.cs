ee110fd673000860a6dc8d8d7c522ab560e2e134
CecilieStroemsnes <cethst@gmail.com>
2025-06-09 19:02:31 +0200

    Updated out.txt

--- homeworks/ODE/main.cs
+++ homeworks/ODE/main.cs
@@ -6,7 +6,6 @@ using System.Globalization;
 
 
 class main {
-
     // Simple harmonic oscillator: y'' = -y
     static Func<double, vector, vector> SHO = (x, y) => new vector(
         y[1],
@@ -33,26 +32,79 @@ class main {
 
     static void Main() {
 
+        Console.WriteLine();
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Task A");
+        Console.WriteLine("=============================================================");
+        Console.WriteLine();
+
+        Console.WriteLine("Simple Harmonic Oscillator:");
+        
         // -------------- SHO ----------------
         var xs = new genlist<double>();
         var ys = new genlist<vector>();
 
         ode.adaptiveDriver(SHO, 0, new vector(0, 1), 4 * PI, xs, ys);
+
+        Console.WriteLine("Steps taken: {0}", xs.size);
+        //Console.WriteLine("Final x: {0}", xs[xs.size - 1]);
+        //Console.WriteLine("Final y(x): {0}", ys[ys.size - 1][0]);
+        Console.WriteLine("Figure: sho.png");
+        Console.WriteLine();
+
         using (var writer = new StreamWriter("sho.txt")) {
             for (int i = 0; i < xs.size; i++)
                 writer.WriteLine($"{xs[i].ToString(CultureInfo.InvariantCulture)} {ys[i][0].ToString(CultureInfo.InvariantCulture)}");
         }
+
+        Console.WriteLine("Damped Harmonic Oscillator:");
 
         // -------------- Damped Oscillator ----------------
         xs = new genlist<double>();
         ys = new genlist<vector>();
 
         ode.adaptiveDriver(DAMP, 0, new vector(0, 1), 4 * PI, xs, ys);
+
+        Console.WriteLine("Steps taken: {0}", xs.size);
+        //Console.WriteLine("Final x: {0}", xs[xs.size - 1]);
+        //Console.WriteLine("Final y(x): {0}", ys[ys.size - 1][0]);
+        Console.WriteLine("Figure: damped.png");
+        Console.WriteLine();
+
         using (var writer = new StreamWriter("damped.txt")) {
             for (int i = 0; i < xs.size; i++)
                 writer.WriteLine($"{xs[i].ToString(CultureInfo.InvariantCulture)} {ys[i][0].ToString(CultureInfo.InvariantCulture)}");
         }
 
+        // task B ------------------------------
+        Console.WriteLine();
+        Console.WriteLine("=============================================================");
+        Console.WriteLine("Task B");
+        Console.WriteLine("=============================================================");
+        Console.WriteLine();
+
+        Console.WriteLine("Newtonian circular orbit:");
+        // -------------- Newtonian Circular Orbit ----------------
+        xs = new genlist<double>();
+        ys = new genlist<vector>();
+
+        ode.adaptiveDriver(NEWTON, 0, new vector(1, 0.01), 12 * PI, xs, ys);
+        Console.WriteLine("Steps taken: {0}", xs.size);
+        //Console.WriteLine("Final φ: {0}", xs[xs.size - 1]);
+        //Console.WriteLine("Final u(φ): {0}", ys[ys.size - 1][0]);
+        Console.WriteLine("Figure: circular_orbit.png");
+        Console.WriteLine();
+
+        using (var writer = new StreamWriter("circular_orbit.txt")) {
+            for (int i = 0; i < xs.size; i++) {
+                double r = 1 / ys[i][0];
+                double x = r * Cos(xs[i]);
+                double y = r * Sin(xs[i]);
+                writer.WriteLine($"{x.ToString(CultureInfo.InvariantCulture)} {y.ToString(CultureInfo.InvariantCulture)}");
+            }
+        }
+
+        Console.WriteLine("Newtonian Elliptical Orbit:");
         // -------------- Newtonian Orbit ----------------
         xs = new genlist<double>();
         ys = new genlist<vector>();
@@ -61,6 +113,13 @@ class main {
         double uprime0 = -0.5;
 
         ode.adaptiveDriver(NEWTON, 0, new vector(u0, uprime0), 4 * PI, xs, ys);
+
+        Console.WriteLine("Steps taken: {0}", xs.size);
+        //Console.WriteLine("Final φ: {0}", xs[xs.size - 1]);
+        //Console.WriteLine("Final u(φ): {0}", ys[ys.size - 1][0]);
+        Console.WriteLine("Figure: newton_orbit.png");
+        Console.WriteLine();
+
         using (var writer = new StreamWriter("newton_orbit.txt")) {
             for (int i = 0; i < xs.size; i++) {
                 double r = 1 / ys[i][0];
@@ -70,11 +129,20 @@ class main {
             }
         }
 
+        Console.WriteLine("Relativistic Orbit with Precession:");
+
         // -------------- Relativistic Orbit ----------------
         xs = new genlist<double>();
         ys = new genlist<vector>();
 
         ode.adaptiveDriver(RELATIVISTIC, 0, new vector(u0, uprime0), 12 * PI, xs, ys);
+
+        Console.WriteLine("Steps taken: {0}", xs.size);
+        //Console.WriteLine("Final φ: {0}", xs[xs.size - 1]);
+        //Console.WriteLine("Final u(φ): {0}", ys[ys.size - 1][0]);
+        Console.WriteLine("Figure: relativistic_orbit.png");
+        Console.WriteLine();
+
         using (var writer = new StreamWriter("relativistic_orbit.txt")) {
             for (int i = 0; i < xs.size; i++) {
                 double r = 1 / ys[i][0];
--- homeworks/ODE/plots.gnuplot
+++ homeworks/ODE/plots.gnuplot
@@ -10,12 +10,23 @@ set output 'damped.png'
 set title 'Damped Harmonic Oscillator'
 plot 'damped.txt' using 1:2 with lines title 'y(x)'
 
-set output 'newton_orbit.png'
+set output 'circular_orbit.png'
 set size square
-set title 'Newtonian Orbit'
+set title 'Newtonian Circular Orbit'
 set xlabel "x"
 set ylabel "y"
-plot 'newton_orbit.txt' using 1:2 with lines title 'orbit'
+set xrange [-1.2:1.2]
+set yrange [-1.2:1.2]
+plot 'circular_orbit.txt' using 1:2 with lines notitle
+
+set output 'newton_orbit.png'
+set size square
+set title 'Newtonian Elliptical Orbit'
+set xlabel "x"
+set ylabel "y"
+set xrange [-2:2]
+set yrange [-2:2]
+plot 'newton_orbit.txt' using 1:2 with lines notitle
 
 set output 'relativistic_orbit.png'
 set size square
